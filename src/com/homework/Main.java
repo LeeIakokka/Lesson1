a15cde0204b1b8f27e6499b2ca4803bba4f46c64
LeeIakokka <mawmiller123@gmail.com>
2015-04-21 22:05:16 +0300

    Initial commit

--- src/com/homework/Main.java
+++ src/com/homework/Main.java
@@ -3,8 +3,12 @@ package com.homework;
 
 public class Main {
     public static void main(String[] args) {
+        int index = 0;
 
-        for (String argument : args) System.out.println(argument);
-
+        for (String argument : args) {
+            System.out.println(argument);
+            index++;
+        }
+        System.out.println(index);
     }
 }
